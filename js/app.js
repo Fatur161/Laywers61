27321874d588f7db61044d56efc11882e8efd332
Daniil Semenov <danil_semenov18@mail.ru>
2023-12-07 21:18:09 +0300

    Del php

--- js/app.js
+++ js/app.js
@@ -1,69 +1,69 @@
-$('#form-laywers').on('submit', function (event) {
+// $('#form-laywers').on('submit', function (event) {
 
-    event.stopPropagation();
-    event.preventDefault();
+//     event.stopPropagation();
+//     event.preventDefault();
 
-    let form = this,
-        submit = $('.submit', form),
-        data = new FormData(),
-        files = $('input[type=file]')
-
-
-    $('.submit', form).val('Отправка...');
-    $('input, textarea', form).attr('disabled','');
-
-    data.append( 'name', 		$('[name="name"]', form).val() );
-    data.append( 'email', 		$('[name="email"]', form).val() );
-    data.append( 'tel', 		$('[name="tel"]', form).val() );
-    data.append( 'category', 		$('[name="select-category"]', form).val() );
-    data.append( 'message', 		$('[name="message"]', form).val() );
+//     let form = this,
+//         submit = $('.submit', form),
+//         data = new FormData(),
+//         files = $('input[type=file]')
 
 
+//     $('.submit', form).val('Отправка...');
+//     $('input, textarea', form).attr('disabled','');
+
+//     data.append( 'name', 		$('[name="name"]', form).val() );
+//     data.append( 'email', 		$('[name="email"]', form).val() );
+//     data.append( 'tel', 		$('[name="tel"]', form).val() );
+//     data.append( 'category', 		$('[name="select-category"]', form).val() );
+//     data.append( 'message', 		$('[name="message"]', form).val() );
 
 
 
-    files.each(function (key, file) {
-        let cont = file.files;
-        if ( cont ) {
-            $.each( cont, function( key, value ) {
-                data.append( key, value );
-            });
-        }
-    });
 
-    $.ajax({
-        url: 'ajax.php',
-        type: 'POST',
-        data: data,
-        cache: false,
-        dataType: 'json',
-        processData: false,
-        contentType: false,
-        xhr: function() {
-            let myXhr = $.ajaxSettings.xhr();
 
-            if ( myXhr.upload ) {
-                myXhr.upload.addEventListener( 'progress', function(e) {
-                    if ( e.lengthComputable ) {
-                        let percentage = ( e.loaded / e.total ) * 100;
-                            percentage = percentage.toFixed(0);
-                        $('.submit', form)
-                            .html( percentage + '%' );
-                    }
-                }, false );
-            }
+//     files.each(function (key, file) {
+//         let cont = file.files;
+//         if ( cont ) {
+//             $.each( cont, function( key, value ) {
+//                 data.append( key, value );
+//             });
+//         }
+//     });
 
-            return myXhr;
-        },
-        error: function( jqXHR, textStatus ) {
-            // Тут выводим ошибку
-        },
-        complete: function() {
-            // Тут можем что-то делать ПОСЛЕ успешной отправки формы
-            console.log('Complete')
-            form.reset()
-        }
-    });
+//     $.ajax({
+//         url: 'ajax.php',
+//         type: 'POST',
+//         data: data,
+//         cache: false,
+//         dataType: 'json',
+//         processData: false,
+//         contentType: false,
+//         xhr: function() {
+//             let myXhr = $.ajaxSettings.xhr();
 
-    return false;
-});
+//             if ( myXhr.upload ) {
+//                 myXhr.upload.addEventListener( 'progress', function(e) {
+//                     if ( e.lengthComputable ) {
+//                         let percentage = ( e.loaded / e.total ) * 100;
+//                             percentage = percentage.toFixed(0);
+//                         $('.submit', form)
+//                             .html( percentage + '%' );
+//                     }
+//                 }, false );
+//             }
+
+//             return myXhr;
+//         },
+//         error: function( jqXHR, textStatus ) {
+//             // Тут выводим ошибку
+//         },
+//         complete: function() {
+//             // Тут можем что-то делать ПОСЛЕ успешной отправки формы
+//             console.log('Complete')
+//             form.reset()
+//         }
+//     });
+
+//     return false;
+// });
